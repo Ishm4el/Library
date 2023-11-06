1bd69dd6df2d858138fef2bdfe226669ab4641b3
Ishii <ismaelv@protonmail.com>
2023-11-06 01:23:58 -0800

    Completed the Library Project of the Odin Project. It has completely operating utilities, including: the option to add additional books, a button to remove a book, a button to toggle the read status of a book.

--- script.js
+++ script.js
@@ -3,13 +3,24 @@ function Book(title, author, pages, haveRead) {
 	this.author = author;
 	this.pages = pages;
 	this.haveRead = haveRead;
-	this.info = function() {
-		if (haveRead) {
-			return title + ' by ' + author + ', ' + pages + ', has read';
-		}
-		return title + ' by ' + author + ', ' + pages + ', not read yet';
-	};
 }
+
+Book.prototype = { 
+    info : function() {
+        if (haveRead) {
+            return title + ' by ' + author + ', ' + pages + ', has read';
+        }
+        return title + ' by ' + author + ', ' + pages + ', not read yet';
+    },
+    toggleHaveRead : function() {
+        if (this.haveRead === true)
+            this.haveRead = false;
+        else
+            this.haveRead = true;
+        return this.haveRead;
+        // updateLibraryDisplay();
+    }
+};
 
 const myLibrary = [new Book('The Hobbit', 'J.R.R. Tolkien', 295, false), 
                     new Book('Anna Karenina', 'Leo Tolstoy', 895, true),
@@ -21,36 +32,77 @@ const myLibrary = [new Book('The Hobbit', 'J.R.R. Tolkien', 295, false),
                     new Book('Moby Dick', 'Herman Melville', 426, true),
                     new Book('Anna Karenina', 'Leo Tolstoy', 895, true),
                     new Book('Moby Dick', 'Herman Melville', 426, true),];
-console.log(myLibrary.length);
 
-function addBookToLibrary() {
-    
+function addBookToLibrary(title, author, pages, read) {
+    myLibrary.push(new Book(title, author, pages, read));
+    updateLibraryDisplay(myLibrary);
 }
 
-function updateLibraryDisplay(myLibrary) {
-    const display = document.querySelector('#display');
-    myLibrary.forEach(element => {
-        const div = document.createElement('div');
-        div.classList.add('card');
-        console.log(element.haveRead === true ? "Read" : "Haven't Read");
-        div.innerHTML =
-            `<div class="book-title">${element.title}</div>
-                <div class="book-info">
-                    <div class="book-author">${element.author}</div>
-                    <div class="book-pages">${element.pages} pages</div>
-                    <div class="book-read">${element.haveRead === true ? "Read" : "Haven't Read"}</div>
+const display = document.querySelector('#display');
+function appendBookToDisplay(title, author, pages, haveRead, i) {
+    const readStatus = haveRead;
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML =
+        `<div class="book-title">${title}</div><button class="remove-button">X</button>
+            <div class="book-info">
+                <div class="book-author">${author}</div>
+                <div class="book-pages">${pages} pages</div>
+                <div class="book-read">
+                    <span>${readStatus ? "Read" : "Haven't Read"}</span>
+                    <button class="toggle-read" value="${i}">◄</button>
                 </div>
-            </div>`;
-        display.appendChild(div);
+            </div>
+        </div>`;
+    div.querySelector("button").addEventListener("click", () => {removeBookFromLibrary(i)});
+    div.querySelector(".toggle-read").addEventListener("click", (e) => {
+        div.querySelector("span").textContent = myLibrary[i].toggleHaveRead() ? "Read" : "Haven't Read";
     });
+    display.appendChild(div);    
+}
+
+function updateLibraryDisplay() {
+    display.innerHTML = '';
+    myLibrary.forEach((element, i) => {
+        appendBookToDisplay(element.title, element.author, element.pages, element.haveRead, i);
+    }); 
 }
 
 const showButton = document.getElementById("showDialog");
 const addBookDialog = document.getElementById("addDialog");
 showButton.addEventListener("click", () => {
     addBookDialog.showModal();
-})
+});
+
+const confirmButton = document.getElementById("confirmButton");
+const theForm = document.querySelector("form");
+confirmButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (theForm.checkValidity()) {
+        addBookToLibrary(theForm.elements["bookTitle"].value,
+            theForm.elements["bookAuthor"].value,
+            theForm.elements["bookPages"].value,
+            theForm.elements["book-read"].value);
+            addBookDialog.close();
+            theForm.reset();
+    }
+    else
+        alert("Please fill all the inputs");
+});
+
+const cancelButton = document.getElementById("cancelButton");
+const allInputs = document.querySelectorAll("input");
+cancelButton.addEventListener("click", () => {
+    addBookDialog.close();
+    theForm.reset();
+        // allInputs.forEach((anInput) => {
+        //     anInput.innerText = '';
+        // });
+});
+
+function removeBookFromLibrary(index) {
+    myLibrary.splice(index, 1);
+    updateLibraryDisplay(myLibrary);
+}
 
 updateLibraryDisplay(myLibrary);
-
-
